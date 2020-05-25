e2c269bf76cf0168c89366b16f8becfb249e51e2
iourisorokine <isorokine@gmail.com>
2020-05-25 21:49:00 +0200

    update build

---
+++ client/build/precache-manifest.3f73ea7139ab95e0e0d8dbf29a409d1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81948bed8b3ce045ff6424fa7f13be1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7df09c23a220a2bf6eb",
+    "url": "/static/css/main.61f9864a.chunk.css"
+  },
+  {
+    "revision": "dc30578da79f0c2c885c",
+    "url": "/static/js/2.9567552f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9567552f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7df09c23a220a2bf6eb",
+    "url": "/static/js/main.4a7bbcf9.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
